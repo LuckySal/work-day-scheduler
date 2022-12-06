5ad3e0d6154a044426c2bb90490e6c66aa256d05
Brandon <hillbp2014@gmail.com>
2022-12-05 20:10:12 -0500

    data is saved and loaded from localstorage

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Temporary
 const currentTime = "2022-12-05T11:45:21";
-let timeArray = $(".time-block").toArray();
+let timeElements = $(".time-block");
 
 function init() {
   // Set the current time and keep it updated
@@ -8,7 +8,9 @@ function init() {
   colorTime();
 
   setInterval(setTime, 1000);
-  // setInterval(colorTime, 1000);
+  setInterval(colorTime, 1000);
+
+  getLocal();
 }
 
 function setTime() {
@@ -20,7 +22,7 @@ function setTime() {
 function colorTime() {
   let now = moment(currentTime).startOf("hour");
 
-  timeArray.forEach((element) => {
+  timeElements.each((index, element) => {
     let hour = moment(currentTime).startOf("date").add(element.id, "h");
     if (hour.isBefore(now)) {
       element.children[1].className = "past";
@@ -32,4 +34,19 @@ function colorTime() {
   });
 }
 
+function getLocal() {
+  timeElements.each((index, element) => {
+    element.children[1].value = localStorage.getItem(element.id);
+  });
+}
+
+function handleSaveTimeblock(event) {
+  let time = event.target.parentElement.parentElement.id;
+  let text = event.target.parentElement.parentElement.children[1].value;
+  localStorage.setItem(time, text);
+  getLocal();
+}
+
 init();
+
+$(".time-block").on("click", ".fa-save", handleSaveTimeblock);
